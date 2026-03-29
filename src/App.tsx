75ceda6ce8328f229a41a15ab2d01a4d26988195
Sumanth Tatipamula <sumanthtatipamula999@gmail.com>
2026-03-29 23:43:08 +0530

    chore: initial project setup with React, TypeScript, Tailwind CSS, Vite

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-950 text-white font-[Inter]">
+      <p className="text-center pt-20 text-2xl">Portfolio coming soon...</p>
+    </div>
+  )
+}
+
+export default App
